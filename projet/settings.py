f4671a3ec09fc914930a5a7b5caad4574e8cefd0
Angelo <letresordedowa78@gmail.com>
2025-12-12 22:17:53 +0100

    settting

--- projet/settings.py
+++ projet/settings.py
@@ -3,16 +3,31 @@ import os
 from decouple import config
 import dj_database_url
 
+# --- BASE ---
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_URL = 'static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# --- SECURITY ---
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = True
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".ngrok-free.app", "*"]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://*.ngrok-free.app',
+]
+
+# --- DATABASE ---
+DATABASES = {
+    'default': dj_database_url.parse(config('DATABASE_URL'))
+}
+
+# --- APPS ---
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -53,25 +68,30 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'projet.wsgi.application'
 
-DATABASES = {
-    'default': dj_database_url.parse(config('DATABASE_URL'))
-}
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'Africa/Porto-Novo'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+LOGIN_URL = '/enseignant/login/'
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# --- INFOBIP (optionnel) ---
+INFOBIP_BASE_URL = config('INFOBIP_BASE_URL', default='')
+INFOBIP_API_KEY = config('INFOBIP_API_KEY', default='')
+INFOBIP_SENDER = config('INFOBIP_SENDER', default='AcadyApp')
 
-from decouple import config
-
-EMAIL_BACKEND = config("EMAIL_BACKEND")
-EMAIL_HOST = config("EMAIL_HOST")
-EMAIL_PORT = config("EMAIL_PORT", cast=int)
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
+# --- EMAIL (Gmail) ---
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_PORT = config('EMAIL_PORT', cast=int, default=587)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool, default=True)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
